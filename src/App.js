cad5cc27f4a9f843322b2f0aab6065bec901c8e4
Younes Lassiri <youneslassirifreelance@gmail.com>
2024-10-27 15:48:38 +0100

    'stats'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Featured from "./Featured/Featured";
+import Stats from "./Stats/Stats";
 function App() {
   return (
     <div className="app">
       <Featured/>
+      <Stats/>
     </div>
   );
 }
