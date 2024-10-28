8244e943362e10d44951683ac2b189dc437c303e
Younes Lassiri <youneslassirifreelance@gmail.com>
2024-10-28 13:41:15 +0100

    'changes'

--- src/Home/nav/Nav.jsx
+++ src/Home/nav/Nav.jsx
@@ -5,21 +5,19 @@ import { BiX } from 'react-icons/bi';
 
 const Nav = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-
   return (
     <div>
       {/* Large screens */}
-      <div className='hidden md:flex justify-between px-[8rem] items-center h-[72px] '>
-        <div>BookYourGame</div>
+      <div className='hidden md:flex justify-between px-[50px] items-center h-[72px]'>
+        <div>Yallah9asser</div>
         <div className='space-x-5'>
-          <button className='bg-gray-100 p-2 rounded-[4px] font-semibold'>Register</button>
-          <button className='bg-red-600 text-white p-2 rounded-[4px] font-semibold'>Sign In</button>
+        <button className='p-2 px-[16px] rounded-[4px] font-medium text-[14px]' style={{ color: '#01031e', backgroundColor: '#eef0fc' }}>Register</button>
+        <button className='bg-red-600 text-white p-2 px-[16px] rounded-[4px] font-medium text-[14px]'>Sign In</button>
         </div>
       </div>
-
       {/* Small screens */}
-      <div className='flex items-center justify-between md:hidden p-4 '>
-        <div>BookYourGame</div>
+      <div className='flex items-center justify-between md:hidden px-[25px] h-[72px]'>
+        <div>Yallah9asser</div>
         <button
           onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
           className=' focus:outline-none'
@@ -27,7 +25,15 @@ const Nav = () => {
           {isMobileMenuOpen ? (
             <BiX className='w-6 h-6' />
           ) : (
-            <FaBars className='w-6 h-6' />
+            <div 
+            className='p-[9px]' 
+            style={{ transition: 'background-color 0.3s', borderRadius: '5px' }} 
+            onMouseEnter={(e) => e.currentTarget.style.backgroundColor = '#eef0fc'} 
+            onMouseLeave={(e) => e.currentTarget.style.backgroundColor = 'transparent'}
+          >
+            <FaBars className='w-5 h-5' />
+          </div>
+
           )}
         </button>
       </div>
@@ -35,9 +41,9 @@ const Nav = () => {
       {/* Mobile Menu */}
       {isMobileMenuOpen && (
         <div className='md:hidden mx-4 pb-4'>
-          <div className='flex flex-col  p-3 font-semibold rounded-md gap-2'>
-            <button className='bg-gray-200 p-2 rounded-[4px] font-semibold'>Register</button>
-            <button className='bg-red-600 text-white p-2 rounded-[4px] font-semibold'>Sign In</button>
+          <div className='flex flex-col font-semibold rounded-md gap-2 py-3'>
+          <button className='py-2 px-[16px] rounded-[4px] font-medium text-[14px]' style={{ color: '#01031e', backgroundColor: '#eef0fc' }}>Register</button>
+        <button className='bg-red-600 text-white py-2 px-[16px] rounded-[4px] font-medium text-[14px]'>Sign In</button>
           </div>
 
           <div className='pt-2 flex flex-col space-y-4 font-semibold'>
